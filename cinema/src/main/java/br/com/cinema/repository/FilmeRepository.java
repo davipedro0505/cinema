fe4ada320b5d782ac88fd9de5bd8a9a78757a641
davipedro0505 <davipedrosilvaalves@gmail.com>
2025-12-11 12:41:29 -0300

    Update FilmeRepository.java

--- cinema/src/main/java/br/com/cinema/repository/FilmeRepository.java
+++ cinema/src/main/java/br/com/cinema/repository/FilmeRepository.java
@@ -2,6 +2,10 @@ package br.com.cinema.repository;
 
 import br.com.cinema.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+    List<Filme> findByTituloContainingIgnoreCase(String titulo);
+
 }
