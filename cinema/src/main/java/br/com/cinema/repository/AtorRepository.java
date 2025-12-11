03074ab2f8be1d5987bf4347408a53994f407f3e
davipedro0505 <davipedrosilvaalves@gmail.com>
2025-12-11 12:40:24 -0300

    Update AtorRepository.java

--- cinema/src/main/java/br/com/cinema/repository/AtorRepository.java
+++ cinema/src/main/java/br/com/cinema/repository/AtorRepository.java
@@ -3,5 +3,8 @@ package br.com.cinema.repository;
 import br.com.cinema.model.Ator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AtorRepository extends JpaRepository<Ator, Long> {
+    List<Ator> findByNomeContainingIgnoreCase(String nome);
 }
